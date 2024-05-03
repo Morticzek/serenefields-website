3cda8b8a3d2bd4ed6f198c0b0f41701be49df06f
Morticzek <48515330+Morticzek@users.noreply.github.com>
2024-05-03 18:12:24 +0200

    shadcn setup

---
+++ src/components/widgets/header.tsx
@@ -0,0 +1,30 @@
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+export default function Header() {
+    return (
+        <header>
+            <div className="flex justify-center items-center">
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <NavigationMenuLink>Link</NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+        </header>
+    )
+}
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+import type { HTMLAttributes, ImageMetadata } from 'astro/types';
+
+let site = {
+    title: string,
+    description: string,
+    url: string,
+}
