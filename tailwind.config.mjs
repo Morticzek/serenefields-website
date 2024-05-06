1909a4468f4c541d48c2fd6592cb713de81de3e4
Morticzek <48515330+Morticzek@users.noreply.github.com>
2024-05-06 22:06:47 +0200

    Custom component button setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,19 +6,26 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
-  prefix: "sh-",
+  prefix: "",
   theme: {
     container: {
       center: true,
       padding: "2rem",
       screens: {
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1280px",
         "2xl": "1400px",
       },
     },
     extend: {
       colors: {
+        "discord-blurple": "#5865F2",
+
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
