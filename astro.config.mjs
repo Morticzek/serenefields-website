f69c22882f434492b76c185771ed6640f6014e73
Morticzek <48515330+Morticzek@users.noreply.github.com>
2024-05-21 10:04:52 +0200

    Port set

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 8080
+  },
   integrations: [react(),
     tailwind({
       applyBaseStyles: false,
