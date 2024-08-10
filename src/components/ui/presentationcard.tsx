eddc6c3faf8e786880a534ae90a6f3c6c0bba118
Morticzek <48515330+Morticzek@users.noreply.github.com>
2024-08-10 19:10:40 +0200

    Remove MivosVanos, add MarkisPL, PhobicShoe, SpideyZac

--- src/components/ui/presentationcard.tsx
+++ src/components/ui/presentationcard.tsx
@@ -8,82 +8,96 @@ import styles from '../../assets/styles/components/presentationcard.module.css';
 
 import FrozenmanHead from '../../assets/image/skins/frozenman_head.png?url';
 import MorticzekHead from '../../assets/image/skins/morticzek_head.png?url';
-import MivosVanosHead from '../../assets/image/skins/mivosvanos_head.png?url';
 import QxertyHead from '../../assets/image/skins/qxerty_head.png?url';
 import JustWhenHead from '../../assets/image/skins/justwhen_head.png?url';
 import Chl0evHead from '../../assets/image/skins/chl0ev_head.png?url';
 import SimonZoliHead from '../../assets/image/skins/simonzoli_head.png?url';
 import xNova204Head from '../../assets/image/skins/xnova204_head.png?url';
 import Anidiotnon_Head from '../../assets/image/skins/anidiotnon__head.png?url';
+import MarkisPLHead from '../../assets/image/skins/markispl_head.png?url';
+import SpideyZacHead from '../../assets/image/skins/spideyzac_head.png?url';
+import PhobicShoeHead from '../../assets/image/skins/phobicshoe_head.png?url';
 
 import Frozenman from '../../assets/image/skins/frozenman.webp?url';
 import Morticzek from '../../assets/image/skins/morticzek.webp?url';
-import MivosVanos from '../../assets/image/skins/mivosvanos.webp?url';
 import Qxerty from '../../assets/image/skins/qxerty.webp?url';
 import JustWhen from '../../assets/image/skins/justwhen.webp?url';
 import Chl0ev from '../../assets/image/skins/chl0ev.webp?url';
 import SimonZoli from '../../assets/image/skins/simonzoli.webp?url';
 import xNova204 from '../../assets/image/skins/xnova204.webp?url';
 import Anidiotnon from '../../assets/image/skins/anidiotnon_.webp?url';
+import MarkisPL from '../../assets/image/skins/markispl.webp?url';
+import SpideyZac from '../../assets/image/skins/spideyzac.webp?url';
+import PhobicShoe from '../../assets/image/skins/phobicshoe.webp?url';
 
 const heads = {
     frozenman: FrozenmanHead,
     morticzek: MorticzekHead,
     anidiotnon: Anidiotnon_Head,
-    mivosvanos: MivosVanosHead,
+    spideyzac: SpideyZacHead,
+    phobicshoe: PhobicShoeHead,
     justwhen: JustWhenHead,
     qxerty: QxertyHead,
     chl0ev: Chl0evHead,
     xnova204: xNova204Head,
     simonzoli: SimonZoliHead,
+    markispl: MarkisPLHead
 };
 
 const images: Record<string, string> = {
     frozenman: Frozenman,
     morticzek: Morticzek,
     anidiotnon: Anidiotnon,
-    mivosvanos: MivosVanos,
     justwhen: JustWhen,
     qxerty: Qxerty,
     chl0ev: Chl0ev,
     xnova204: xNova204,
     simonzoli: SimonZoli,
+    markispl: MarkisPL,
+    spideyzac: SpideyZac,
+    phobicshoe: PhobicShoe
 };
 
 const names: Record<string, string> = {
     frozenman: 'Frozenman',
     morticzek: 'Morticzek',
-    mivosvanos: 'MivosVanos',
     qxerty: 'qxerty',
     justwhen: 'JustWhen',
     chl0ev: 'chl0ev',
     simonzoli: 'simonzoli99',
     xnova204: 'xNova204',
     anidiotnon: 'Anidiotnon_',
+    markispl: 'MarkisPL',
+    spideyzac: 'SpideyZac',
+    phobicshoe: 'PhobicShoe'
 };
 
 const descriptions: Record<string, string> = {
-    frozenman: 'Hey there, I’m Frozenman! As the owner of the server and a software engineer, I’m doing my best to make a fun game for everyone to enjoy. Let’s build something amazing together!',
-    morticzek: 'Hey, I’m Morticzek! I handle website and game development, ensuring everything works seamlessly. I’m thrilled to be part of this awesome team and hope you enjoy your time with us!',
-    mivosvanos: 'Hi, I’m MivosVanos! I specialize in Minecraft map building, and I love bringing creative worlds to life. Can’t wait for you to explore them!',
-    qxerty: 'Hi there, I’m qxerty! I’m the storyline writer and game master, weaving narratives that keep you on the edge of your seat. Get ready for some unforgettable adventures!',
-    justwhen: 'Hello, I’m JustWhen! As the manager and storyline creator, I craft the adventures and keep everything running smoothly. Let’s dive into some epic tales!',
-    chl0ev: 'Hey, I’m Chloe! I’m both a designer and map builder, blending aesthetics with creativity to make our world visually stunning. Excited for you to see what we’ve made!',
-    simonzoli: 'Hi, I’m simonzoli99! I create the music that brings our game to life, setting the mood for your epic adventures. Let\'s make some great memories together!',
-    xnova204: 'Hey, I’m xNova204! I’m a texture artist, and I love creating unique visuals that make our game stand out. Can’t wait for you to see the world we’ve built!',
-    anidiotnon: 'Hey, I’m Anidiotnon_! I’m a game developer, and I’m passionate about creating a fun and engaging experience for all players. Let’s make something awesome together!',
+    frozenman: "Hey, I'm Frozenman! I own the server and work as a software engineer. I'm doing my best to make an awesome server for everyone to enjoy. Let's create something cool together!",
+    morticzek: "Hey there, I’m Morticzek! I take care of the website and server development, making sure everything runs smoothly. I’m stoked to be part of this awesome team, and I hope you have a blast with us!",
+    qxerty: "Greetings! I’m qxerty, the one behind the storylines and game mastering. My goal is to weave tales that keep you hooked. Get ready for some unforgettable adventures on our server!",
+    justwhen: "Hi! I’m JustWhen, and I’m all about creating fun quests and challenges as a game master. I hope you have a fantastic time exploring everything our server has to offer!",
+    chl0ev: "Hello, I’m Chloe! I’m the designer and map builder, blending creativity with aesthetics to make our server look stunning. Can’t wait for you to see what we’ve crafted!",
+    simonzoli: "Hey, I’m simonzoli99! I create the music that sets the mood for your adventures. Together, let’s make some great memories on the server!",
+    xnova204: "Hi, I’m xNova204! I’m a texture artist who loves crafting unique visuals that make our server stand out. I’m excited for you to explore the world we’ve built!",
+    anidiotnon: "Hey there, I’m Anidiotnon_! I’m a server developer with a passion for creating a fun and engaging experience for everyone. Let’s make something awesome together!",
+    markispl: "Howdy! I’m MarkisPL, a builder who loves bringing our world to life with cool structures. I hope you enjoy exploring everything we’ve put together on the server!",
+    spideyzac: "Hi, I’m SpideyZac! As a junior developer, I’m thrilled to be part of the team working on this server. Let’s create something amazing together!",
+    phobicshoe: "Hello! I’m PhobicShoe, a junior developer who’s excited to help bring this server to life. Let’s build something great together!"
 };
 
 const roles: Record<string, string> = {
     frozenman: 'Owner',
     morticzek: 'Developer',
-    mivosvanos: 'Builder',
     qxerty: 'Game Master',
-    justwhen: 'Manager',
+    justwhen: 'Game Master',
     chl0ev: 'Game Master',
     simonzoli: 'Music Composer',
     xnova204: 'Texture Artist',
-    anidiotnon: 'Developer',
+    markispl: 'Builder',
+    anidiotnon: 'Junior Developer',
+    spideyzac: 'Junior Developer',
+    phobicshoe: 'Junior Developer'
 };
 
 const isContentTeam = (role: string) => {
